92595ac1e2a3dcf6c5838293d5169c822b6cbe6e
r151149 <45209359+r151149@users.noreply.github.com>
2022-02-26 00:12:45 +0530

    added logger

--- general/logger.js
+++ general/logger.js
@@ -5,7 +5,8 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console()
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
