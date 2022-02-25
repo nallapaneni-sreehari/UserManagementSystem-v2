269e546d906065131f570506b41d3aabec2b24b9
r151149 <45209359+r151149@users.noreply.github.com>
2022-02-26 00:56:40 +0530

    added logger

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const logger = require('./general/logger');
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 app.use(serveStatic(__dirname + "/dist/TataElxsi"));
+app.use(cors());
 
 app.all('*', (req, res) => {
     res.sendFile(__dirname + '/dist/TataElxsi/index.html');
