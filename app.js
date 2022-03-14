edabb5cb43bd6dfc2267265906c44acad14a2f2c
r151149 <45209359+r151149@users.noreply.github.com>
2022-03-14 22:48:15 +0530

    push service  added

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const serveStatic = require('serve-static');
 const logger = require('./general/logger');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 app.use(serveStatic(__dirname + "/dist/TataElxsi"));
 app.use(cors());
+
+app.get("/worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "worker.js"));
+  });
 
 app.all('*', (req, res) => {
     res.sendFile(__dirname + '/dist/TataElxsi/index.html');
